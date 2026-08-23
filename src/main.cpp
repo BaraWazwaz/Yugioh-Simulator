90cbf0816692930196d36beb40b1ac1f75948a3f
BaraWazwaz <wazwazbaraa@gmail.com>
2026-08-23 16:07:53 +0300

    added:  class

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
+#include <string>
 #include <fmt/core.h>
-#include <fmt/color.h>
+#include <yugioh/card.hpp>
 
 int main() {
-    fmt::print("Hello, Yu-Gi-Oh Simulator Developer!\n");
-    fmt::print("Testing framework version: {} (Released: {})\n", 11.0, 2024);    
-    fmt::print(fg(fmt::color::green) | fmt::emphasis::bold, "✅ Success: fmt library is working flawlessly!\n");
+    using namespace std::string_literals;
+
+    yugioh::Card dark_magician ("Dark Magician"s, "some url"s);
+    fmt::print("{}: {}\n", dark_magician.name(), dark_magician.imageURL());
     return 0;
 }
